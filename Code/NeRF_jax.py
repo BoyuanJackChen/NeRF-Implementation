4a60fc901b79f2a9464280d00714e74a67f4ea72
loan <bcaa2017@mymail.pomona.edu>
2020-08-04 10:40:30 +0800

    aha

--- Code/NeRF_jax.py
+++ Code/NeRF_jax.py
@@ -15,7 +15,7 @@ import os
 from load_llff import *
 
 slash = '/' if (platform == "darwin" or platform == "linux") else '\\'
-BASE_DIR = sys.path[0] + "/.."
+BASE_DIR = sys.path[0] + "/../.."
 DATA_DIR = BASE_DIR + '/Dataset'
 CODE_DIR = BASE_DIR + "/Code"
 LLFF_DATA = DATA_DIR+"/nerf_llff_data"
